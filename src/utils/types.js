c5f2fcea40e9d76bca8f2ca914a75fcf2dee42f9
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-18 20:34:27 +0200

    *

---
+++ src/utils/types.js
@@ -0,0 +1,7 @@
+export const types = {
+  NumericLiteral: 'NumericLiteral',
+  StringLiteral: 'StringLiteral',
+  BlockStatement: 'BlockStatement',
+  ExpressionStatement: `ExpressionStatement`,
+  Program: `Program`,
+};
