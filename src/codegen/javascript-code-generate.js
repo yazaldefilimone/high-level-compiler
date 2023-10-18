9b14243132028498a536cbe4a927c906e909d5f9
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-18 20:34:13 +0200

    *

--- src/codegen/javascript-code-generate.js
+++ src/codegen/javascript-code-generate.js
@@ -1,16 +1,45 @@
 export class JavaScriptCodeGenerate {
+  constructor(indent = 2) {
+    this._indent = indent;
+    this._currentIndent = indent;
+  }
   generate(expression) {
     return this.gen(expression);
   }
 
   gen(expression) {
-    if (expression.type === null) {
-      throw `Unexpected expression: ${expression.type}`;
+    if (!this[expression.type]) {
+      throw `JavaScriptCodeGenerate - Unexpected expression: ${expression.type}`;
     }
     return this[expression.type](expression);
   }
 
-  NumberLiteral(expression) {
-    return String(expression);
+  NumericLiteral(expression) {
+    return String(expression.value);
+  }
+
+  StringLiteral(expression) {
+    return String(expression.value);
+  }
+
+  BlockStatement(expression) {
+    const code = expression.body.map((exp) => {
+      return `\n${this._indenting() + this.gen(exp)}`;
+    });
+    return `{` + code.join('') + '\n' + `}`;
+  }
+  ExpressionStatement({ expression }) {
+    return `${this.gen(expression)};`;
+  }
+  Program({ body }) {
+    return `${this.gen(body)}`;
+  }
+
+  _indenting() {
+    let spaces = '';
+    for (let index = 0; index < this._currentIndent; index++) {
+      spaces += ' ';
+    }
+    return spaces;
   }
 }
