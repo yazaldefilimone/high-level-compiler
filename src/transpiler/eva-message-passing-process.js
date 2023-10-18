2da3e8d0475b242c712af53096086cbbf22f57f1
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-18 20:34:19 +0200

    *

--- src/transpiler/eva-message-passing-process.js
+++ src/transpiler/eva-message-passing-process.js
@@ -1,15 +1,14 @@
 import { JavaScriptCodeGenerate } from '../codegen';
+import { types } from '../utils';
 
 const javaScriptCodeGenerate = new JavaScriptCodeGenerate();
 
 export class EvaMessagePassingProcess {
   compile(program) {
-    const evaAst = program;
+    const evaAst = ['begin', program];
+    const javaScriptAst = this._generateProgram(evaAst);
 
-    const javaScriptAst = this.generate(evaAst);
-
-    const target = javaScriptCodeGenerate.gen(javaScriptAst);
-
+    const target = javaScriptCodeGenerate.generate(javaScriptAst);
     this.saveToFile('./out.js', target);
 
     return { ast: javaScriptAst, target };
@@ -21,16 +20,56 @@ export class EvaMessagePassingProcess {
 
     if (this._isNumber(expression)) {
       return {
-        type: 'NumberLiteral',
+        type: types.NumericLiteral,
         value: expression,
+      };
+    }
+
+    if (this._isString(expression)) {
+      return {
+        type: types.StringLiteral,
+        value: expression,
+      };
+    }
+
+    if (expression[0] === 'begin') {
+      const [_tag, ...expressions] = expression;
+      const body = [];
+      expressions.forEach((element) => {
+        body.push(this._toStatement(this.generate(element)));
+      });
+      return {
+        type: types.BlockStatement,
+        body,
       };
     }
 
     throw `Unexpected implemented: ${JSON.stringify(expression)}`;
   }
+  _generateProgram(expression) {
+    const [_tag, expressions] = expression;
+    return {
+      type: 'Program',
+      body: this.generate(expressions),
+    };
+  }
 
   _isNumber(expression) {
     return typeof expression === 'number';
   }
+
+  _isString(expression) {
+    return typeof expression === 'string' && expression.at(0) === '"' && expression.at(-1) === '"';
+  }
+  _toStatement(expression) {
+    switch (expression.type) {
+      case types.NumericLiteral:
+      case types.StringLiteral:
+        return { type: types.ExpressionStatement, expression };
+      default:
+        return expression;
+    }
+  }
+
   saveToFile(filename, target) {}
 }
