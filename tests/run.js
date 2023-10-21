55c47dfb2f663f1ca7b27fe994421c454d859d0e
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-21 15:54:21 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -1,6 +1,11 @@
-import { EvaMessagePassingProcess } from '../src/transpiler/eva-message-passing-process.js';
-const evaMessagePassingProcess = new EvaMessagePassingProcess();
-const { ast, target } = evaMessagePassingProcess.compile('(begin 30 "Hello")');
+import { EvaMPP } from '../src/transpiler/eva-mpp.js';
+const evaMPP = new EvaMPP();
+const { ast, target } = evaMPP.compile(`
+(var user-name 10)
+//(print "user-name:" user-name)
+(set user-name 100)
+//(print "user-name updated:" user-name)
+`);
 
 console.log('---- ast ----');
 console.log(JSON.stringify(ast, null, 2));
