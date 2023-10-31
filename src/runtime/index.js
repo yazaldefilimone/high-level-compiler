b4ae24eea319d85cadc231492c48924aac4db0fd
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-11-01 01:14:56 +0200

    *

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -8,13 +8,21 @@ function print(...args) {
   console.log(...args);
 }
 
-function spawn(fn, ...args) {
-  return scheduler.spawn(fn, args);
+function spawn(internalFunction, ...args) {
+  return scheduler.spawn(internalFunction, args);
 }
 
-async function sleep(ms = 100) {
+async function sleep(ms = 20) {
   return await scheduler.sleep(ms);
 }
 
+function send(receiver, message) {
+  return scheduler.send(receiver, message);
+}
+
+async function receive(receiver) {
+  return await scheduler.receive(receiver);
+}
+
 const NextMath = {};
-module.exports = { print, spawn, sleep, scheduler, NextMath };
+module.exports = { print, spawn, send, receive, sleep, scheduler, NextMath };
