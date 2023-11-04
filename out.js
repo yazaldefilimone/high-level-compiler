6c3c3c516c919603f20fd33064712efdfedaa856
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-11-04 17:15:08 +0200

    *

---
+++ out.js
@@ -0,0 +1,20 @@
+
+// prologue
+// const  {print, spawn, send, receive, sleep, scheduler, NextMath } =  require('../src/runtime');
+
+async function* _handleProcess(el) {
+  while((getWidth(el) < RACE_LENGTH)) {
+    let request = await receive(this);
+    try {
+      let { sender: sender, delay: delay, delta: delta } = request;
+      incWidth(el, delta);
+      let receiver = getRandomProcess();
+      send(receiver, { sender: this, delay: random(50, 200), delta: random(5, 50) });
+      await sleep(delay);
+    } catch (e) {
+      if((e !== NextMath)) throw e 
+    }
+  } 
+}
+
+    
