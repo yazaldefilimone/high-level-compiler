acad95cac6856fc37b4efe27137151ef2b0eb6f6
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-11-06 16:59:45 +0200

    *

--- tests/out.js
+++ tests/out.js
@@ -1,8 +1,9 @@
 
 // prologue
 const  {print, spawn, send, receive, sleep, scheduler, NextMath } =  require('../src/runtime');
+let index = 0;
 while((index < 10)) {
   print("current index: ", index);
-  index = (index + 1);
+  ++index
 } 
     
